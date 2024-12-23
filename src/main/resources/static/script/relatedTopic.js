138f2deeeb5dd197dda906a41e448a17febf6c17
YunSHCode <129275922+YunSHCode@users.noreply.github.com>
2024-12-23 20:27:52 +0900

    redis를 사용하여 캐싱처리

--- src/main/resources/static/script/relatedTopic.js
+++ src/main/resources/static/script/relatedTopic.js
@@ -4,7 +4,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.getElementById("right-refresh").addEventListener("click", () => {
     updateRelatedTopic();
-})
+});
+
+document.getElementById("searchButton").addEventListener("click", () => {
+    console.log("Topic 검색 클릭")
+    var searchInput = $("#searchInput").val();
+    if (!searchInput || searchInput.trim() === "") {
+        return;
+    }
+    $.ajax({
+        type: "GET",
+        url: "http://127.0.0.1:5001/analysticservice/relatedTopics/keyword",
+        data: {keyword: searchInput}
+    }).done((jsondata) => {
+        // 가져온 데이터를 HTML에 표시
+        console.log(jsondata);
+        renderTopicData(jsondata);
+    }).fail((err) => {
+        console.error("데이터 받기 실패", err);
+    });
+
+});
 
 function renderTopicData(data) {
     const container = document.getElementById('right-list');
